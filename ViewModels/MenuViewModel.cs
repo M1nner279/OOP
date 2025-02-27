4fc5e3ed00939b8b1a98a060ecd5d7159ad89aee
Кучко Максим <kuchkomaxim2527@gmail.com>
2025-02-27 22:33:28 +0300

    change project structure

---
+++ ViewModels/MenuViewModel.cs
@@ -0,0 +1,6 @@
+namespace laboop.ViewModels;
+
+public class MenuViewModel : ViewModelBase
+{
+    public string Welcome {get; } = "Hello, there";
+}
---
+++ Views/MenuView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace laboop.Views;
+
+public partial class MenuView : Window
+{
+    public MenuView()
+    {
+        InitializeComponent();
+    }
+}
